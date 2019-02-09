ca46c12ebd69a11611c25b3a9b649a26f7c73b48
Plamena Todorova <plamena.i.todorova@gmail.com>
2019-02-09 23:03:19 +0100

    NewTaskDialog

---
+++ Interface/Dialogs/NewTaskDialog.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Interface.Dialogs
+{
+    /// <summary>
+    /// Interaction logic for NewTaskDialog.xaml
+    /// </summary>
+    public partial class NewTaskDialog : Window
+    {
+        public NewTaskDialog()
+        {
+            InitializeComponent();
+        }
+
+        private void MinimizeWindow(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
+        private void CloseWindow(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
