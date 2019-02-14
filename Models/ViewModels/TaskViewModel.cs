a47a4ed726dc6ab6b0fc133be78bea1e0d449f4f
Plamena Todorova <plamena.i.todorova@gmail.com>
2019-02-15 00:17:55 +0100

    TaskViewModel

---
+++ Models/ViewModels/TaskViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Models.ViewModels
+{
+    public enum ViewType
+    {
+        Work,
+        School,
+        Social,
+        Apointment,
+        Goal
+    }
+
+    public class TaskViewModel
+    {
+        public string Name { get; set; }
+
+        public ViewType Type { get; set; }
+
+        public DateTime Dateline { get; set; }
+
+        public string Description { get; set; }
+    }
+}
