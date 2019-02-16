fbdb241155db60066200e9e403107779a26a18ec
Plamena Todorova <plamena.i.todorova@gmail.com>
2019-02-16 12:32:43 +0100

    Add files via upload

---
+++ Models/DatabaseModels/BaseTask.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Models.DatabaseModels
+{
+    public abstract class BaseTask 
+    {
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+
+        public DateTime DeadLine { get; set; }
+
+        public string Description { get; set; }
+    }
+}
