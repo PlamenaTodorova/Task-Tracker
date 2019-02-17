443ce2d85633590162f4af0d596cf4d9bc3df10f
Plamena Todorova <plamena.i.todorova@gmail.com>
2019-02-17 21:56:25 +0100

    Add files via upload

---
+++ Models/DatabaseModels/Task.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.DatabaseModels
+{
+    public enum TaskType
+    {
+        Work,
+        School,
+        Social,
+        Apointment
+    }
+
+    public class Task : BaseTask
+    {
+        TaskType type { get; set; }
+    }
+}
