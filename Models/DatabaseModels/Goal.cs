f3f72f5d31f334573333d8da2d97a0411e35f8ba
Plamena Todorova <plamena.i.todorova@gmail.com>
2019-02-18 22:27:35 +0100

    Add files via upload

---
+++ Models/DatabaseModels/Goal.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.DatabaseModels
+{
+    public enum Periods
+    {
+        Day,
+        Week,
+        Month,
+        Year
+    }
+
+    public class Goal : BaseTask
+    {
+        public Periods Span { get; set; }
+
+        public uint SuccessfullyFinished { get; set; }
+
+        public uint TotalChalanges { get; set; }
+    }
+}
