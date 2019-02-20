8c104a257b0e3c316a49b9226033dab3b3ba0e7d
Plamena Todorova <plamena.i.todorova@gmail.com>
2019-02-20 23:00:28 +0100

    Add files via upload

---
+++ Models/DatabaseModels/TaskType.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.DatabaseModels
+{
+    public class TaskType
+    {
+        public TaskType()
+        {
+            this.Tasks = new HashSet<Task>();
+        }
+
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string PicturePath { get; set; }
+
+        public ICollection<Task> Tasks { get; set; }
+    }
+}
