66613c5b9aab8ab915422d923b0e71e6348b9c97
Plamena Todorova <plamena.i.todorova@gmail.com>
2019-02-25 15:12:13 +0100

    Add files via upload

---
+++ Interface/Controllers/OneDayController.cs
@@ -0,0 +1,25 @@
+﻿using Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface.Controllers
+{
+    internal class OneDayController : ShowTaskController
+    {
+        private DateTime date;
+
+        public OneDayController(DateTime date)
+        {
+            this.date = date;
+        }
+
+        protected override void GenerateTasks()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
