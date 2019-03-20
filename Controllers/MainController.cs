37ae455dd6ab5aa8ebae215070b34afb714cdded
Plamena Todorova <plamena.i.todorova@gmail.com>
2019-03-20 13:54:16 +0200

    Add files via upload

---
+++ Controllers/MainController.cs
@@ -0,0 +1,18 @@
+﻿using DataStorage;
+using Models.BindingModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface.Controllers
+{
+    public class MainController
+    {
+        public void AddTask(TaskBindingModel model)
+        {
+            Engin.GetEngin().Add(model);
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Interface.Controllers;
+using Interface.Dialogs;
+using Interface.Pages;
+
+namespace Interface
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private MainController controller;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.PageHolder.Navigate(new DayFrame());
+            controller = new MainController();
+        }
+
+        private void MinimizeWindow(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized; 
+        }
+
+        private void CloseWindow(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void SwitchToDaily(object sender, RoutedEventArgs e)
+        {
+            this.PageHolder.Navigate(new DayFrame());
+        }
+
+        private void OpenNewTaskDialog(object sender, RoutedEventArgs e)
+        {
+            NewTaskDialog newTask = new NewTaskDialog();
+
+            newTask.ShowDialog();
+
+            if (newTask.DialogResult == true)
+            {
+                controller.AddTask(newTask.GetTask());
+            }
+        }
+
+        private void SwitchToAllTasks(object sender, RoutedEventArgs e)
+        {
+            this.PageHolder.Navigate(new AllTasks());
+        }
+    }
+}
