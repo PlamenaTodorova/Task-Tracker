60483ba9ec1519ef17a63b8e758a3767d6f8989b
Plamena Todorova <plamena.i.todorova@gmail.com>
2019-08-17 23:59:51 +0300

    Calendar models

---
+++ Models/ViewModels/DayViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.ViewModels
+{
+    public class DayViewModel
+    {
+        public int Date { get; set; }
+
+        public ICollection<DaysTaskViewModel> tasks { get; set; }
+    }
+}
---
+++ Models/ViewModels/DaysTaskViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.ViewModels
+{
+    public class DaysTaskViewModel
+    {
+        public string PicturePath { get; set; }
+
+        public string Title { get; set; }
+    }
+}
